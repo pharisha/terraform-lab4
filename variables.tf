da2f481cc3fbf5a34c491babe4fc0d2dc483469d
pharisha <57386214+pharisha@users.noreply.github.com>
2019-11-09 14:24:56 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,6 +3,16 @@ variable "loc" {
     default     =   "westeurope"
 }
 
+variable "clientsecrets" {
+    description = "client secrets"
+    default     =   "b9543670-34cb-410d-ad75-fb29e77964cc"
+}
+
+variable "subid" {
+    description = "Default Azure region"
+    default     =   "32c32163-f026-41ed-9503-3b0ee6ec5540"
+}
+
 variable "tags" {
     default     = {
         source  = "citadel"
