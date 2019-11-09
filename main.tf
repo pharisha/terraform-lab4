61a197f21c8fe8bd0c4a1d72d93b6c0fb754d163
pharisha <57386214+pharisha@users.noreply.github.com>
2019-11-09 14:24:26 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,6 @@
+provider "azurerm" {
+  subscription_id = "${var.subid}"
+  client_id       = "${var.clientsecrets}"
+  client_secret   = "Jg-xsz_p7Ru/5TOiRke_F1fsimnVUVJ8"
+  tenant_id       = "f66ccb58-5436-40de-ae8b-e951e03cb2b3"
+}
